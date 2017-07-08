0524edafd2058ff9ab8c7eea3d60708921ee3a37
Mattias Harrysson <mathar@kth.se>
2017-07-08 13:18:49 +0200

    rename preprocessor

--- test/volume_test.cpp
+++ test/volume_test.cpp
@@ -19,19 +19,19 @@
 
 using ::testing::DoubleNear;
 
-#define COMPARE(value, eq) ASSERT_THAT(value, DoubleNear(eq, 0.0000001));
+#define CMP_DBL(value, eq) ASSERT_THAT(value, DoubleNear(eq, 0.0000001));
 
 TEST(VolumeTest, StartState)
 {
     Volume v1;
-    COMPARE(v1.get_level(), 1.0);
-    COMPARE(v1.get_minimum(), 0.0);
-    COMPARE(v1.get_maximum(), 1.0);
+    CMP_DBL(v1.get_level(), 1.0);
+    CMP_DBL(v1.get_minimum(), 0.0);
+    CMP_DBL(v1.get_maximum(), 1.0);
 
     Volume v2(-10.0, 100.0);
-    COMPARE(v2.get_level(), 100.0);
-    COMPARE(v2.get_minimum(), -10.0);
-    COMPARE(v2.get_maximum(), 100.0);
+    CMP_DBL(v2.get_level(), 100.0);
+    CMP_DBL(v2.get_minimum(), -10.0);
+    CMP_DBL(v2.get_maximum(), 100.0);
 }
 
 TEST(VolumeTest, IncorrectMinMax)
@@ -46,63 +46,63 @@ TEST(VolumeTest, IncrementDecrement)
 {
     Volume v1;
     v1.decrease();
-    COMPARE(v1.get_level(), 0.8);
+    CMP_DBL(v1.get_level(), 0.8);
     v1.decrease();
-    COMPARE(v1.get_level(), 0.6);
+    CMP_DBL(v1.get_level(), 0.6);
     v1.decrease();
-    COMPARE(v1.get_level(), 0.4);
+    CMP_DBL(v1.get_level(), 0.4);
     v1.decrease();
-    COMPARE(v1.get_level(), 0.2);
+    CMP_DBL(v1.get_level(), 0.2);
     v1.decrease();
-    COMPARE(v1.get_level(), 0.0);
+    CMP_DBL(v1.get_level(), 0.0);
     // locked to minimum
     v1.decrease();
-    COMPARE(v1.get_level(), 0.0);
+    CMP_DBL(v1.get_level(), 0.0);
     v1.increase();
-    COMPARE(v1.get_level(), 0.2);
+    CMP_DBL(v1.get_level(), 0.2);
     v1.increase();
-    COMPARE(v1.get_level(), 0.4);
+    CMP_DBL(v1.get_level(), 0.4);
     v1.increase();
-    COMPARE(v1.get_level(), 0.6);
+    CMP_DBL(v1.get_level(), 0.6);
     v1.increase();
-    COMPARE(v1.get_level(), 0.8);
+    CMP_DBL(v1.get_level(), 0.8);
     v1.increase();
-    COMPARE(v1.get_level(), 1.0);
+    CMP_DBL(v1.get_level(), 1.0);
     // locked to maximum
     v1.increase();
-    COMPARE(v1.get_level(), 1.0);
+    CMP_DBL(v1.get_level(), 1.0);
 
     // different sizes
     Volume v2(-1.0, 2.0, 1.0);
-    COMPARE(v2.get_level(), 2.0);
+    CMP_DBL(v2.get_level(), 2.0);
     v2.decrease();
-    COMPARE(v2.get_level(), 1.0);
+    CMP_DBL(v2.get_level(), 1.0);
     v2.decrease();
-    COMPARE(v2.get_level(), 0.0);
+    CMP_DBL(v2.get_level(), 0.0);
     v2.decrease();
-    COMPARE(v2.get_level(), -1.0);
+    CMP_DBL(v2.get_level(), -1.0);
     // locked to minimum
     v2.decrease();
-    COMPARE(v2.get_level(), -1.0);
+    CMP_DBL(v2.get_level(), -1.0);
     v2.increase();
-    COMPARE(v2.get_level(), 0.0);
+    CMP_DBL(v2.get_level(), 0.0);
     v2.increase();
-    COMPARE(v2.get_level(), 1.0);
+    CMP_DBL(v2.get_level(), 1.0);
     v2.increase();
-    COMPARE(v2.get_level(), 2.0);
+    CMP_DBL(v2.get_level(), 2.0);
     // locked to maximum
     v2.increase();
-    COMPARE(v2.get_level(), 2.0);
+    CMP_DBL(v2.get_level(), 2.0);
 }
 
 TEST(VolumeTest, SetLevel)
 {
     Volume v1;
-    COMPARE(v1.get_level(), 1.0);
+    CMP_DBL(v1.get_level(), 1.0);
     v1.set_level(0.5);
-    COMPARE(v1.get_level(), 0.5);
+    CMP_DBL(v1.get_level(), 0.5);
     v1.set_level(100.0);
-    COMPARE(v1.get_level(), 1.0);
+    CMP_DBL(v1.get_level(), 1.0);
     v1.set_level(-100.0);
-    COMPARE(v1.get_level(), 0.0);
+    CMP_DBL(v1.get_level(), 0.0);
 }
